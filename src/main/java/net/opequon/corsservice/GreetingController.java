ec9b80e9d39758c10da7719f9d7b1e33a2529d5a
Clark Hale <chale@redhat.com>
2020-08-23 18:56:59 -0400

    initial commit

---
+++ src/main/java/net/opequon/corsservice/GreetingController.java
@@ -0,0 +1,39 @@
+package net.opequon.corsservice;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	/*
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}*/
+
+	@CrossOrigin(origins = "http://127.0.0.1")
+	@GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name, @RequestHeader Map<String, String> headers) {
+		System.out.println("==== in greeting ====");
+        headers.forEach((key, value) -> {
+	        System.out.println(String.format("Header '%s' = %s", key, value));
+	    });
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/greeting-javaconfig")
+	public Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
+		System.out.println("==== in greeting ====");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
