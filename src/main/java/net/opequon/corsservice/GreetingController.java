76297fe7adb829db825ecef2c1e764ea82101c85
Clark Hale <chale@redhat.com>
2020-08-25 08:26:26 -0400

    updated cors filter

--- src/main/java/net/opequon/corsservice/GreetingController.java
+++ src/main/java/net/opequon/corsservice/GreetingController.java
@@ -24,7 +24,7 @@ public class GreetingController {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}*/
 
-	@CrossOrigin(origins = {"http://127.0.0.1","https://static-cors-test.apps.cfh-test.sandbox1479.opentlc.com"})
+	@CrossOrigin(origins = {"http://127.0.0.1","https://static.corstest.apps.cfh-test.sandbox1479.opentlc.com"})
 	@GetMapping("/greeting")
     public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name, @RequestHeader Map<String, String> headers, HttpServletRequest hsr) {
 		System.out.println("==== in greeting ====");
